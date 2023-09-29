dd9305608a4e48b801dd3c0cde5d72c053bd307c
Shoeb Shaikh <90shoeb@gmail.com>
2023-09-29 09:55:00 +0800

    adding recommendation files

---
+++ simulate_scenarios.py
@@ -0,0 +1,228 @@
+import pandas as pd
+import plotly.graph_objects as go
+import streamlit as st
+from plotly.subplots import make_subplots
+
+from utils_recommendation import (get_anomaly_recommendation,
+                                  get_defect_recommendation)
+
+st.sidebar.title("Anomaly and defect recommendations")
+
+st.sidebar.title("Set simulation parameters")
+min_percentage = st.sidebar.number_input('Anomaly/Defect Range [%]',value=50.0, min_value=0.0, max_value=100.0)/100.0
+proximity = st.sidebar.number_input('Defect Proximity [m]',value=10.0, min_value=0.0)
+min_severity_improvement = st.sidebar.number_input('SI Improvement [Number]',value=1, min_value=0, max_value=4)
+min_overlap_extent = st.sidebar.number_input('Overlap Extent [m]',value=10.0, min_value=0.0)
+
+
+
+st.subheader('Defect Simulation (Past Inspection)')
+c1_no_of_defects,_,_ = st.columns(3)
+with c1_no_of_defects:
+    no_of_defects = st.number_input('Enter number of defects', value=1, max_value=5, min_value=0)
+
+defect = pd.DataFrame(
+    columns=[
+        "defect_id",
+        "defect_code_id",
+        "line_id",
+        "subsys_id",
+        "payload_id",
+        "measurement_type_id",
+        "defect_status_id",
+        "start_pos",
+        "end_pos",
+        "close_date_timestamp",
+        "user",
+        "modified_dttm",
+    ]
+)
+
+anomaly = pd.DataFrame(
+    columns=[
+        "anomaly_id",
+        "inspection_id",
+        "measurement_type_id",
+        "defect_code_id",
+        "linked_defect_id",
+        "review_status_id",
+        "start_pos",
+        "end_pos",
+        "length",
+        "user",
+        "modified_dttm",
+    ]
+)
+
+defect_start_pos = []
+defect_end_pos = []
+defect_severity = []
+c1,c2,c3 = st.columns(3)
+for defect_no in range(no_of_defects):
+    with c1:
+        start_pos = st.number_input('start_pos',value=0.0,min_value=0.0, key=f'defect_start_{defect_no}')
+        defect_start_pos.append(start_pos)
+    with c2:
+        end_pos = st.number_input('end_pos',value=100.0, key=f'defect_end_{defect_no}')
+        defect_end_pos.append(end_pos)
+    with c3:
+        severity = st.number_input('severity',value=2,min_value=1,max_value=4,key=f'defect_sev_{defect_no}')
+        defect_severity.append(severity)
+
+st.subheader('Anomaly Simulation (Current inspection)')
+
+c1_no_of_anomalies,_, _ = st.columns(3)
+with c1_no_of_anomalies:
+    no_of_anomalies = st.number_input('Enter number of anomalies', value=1, max_value=5, min_value=0)
+
+anomaly_start_pos = []
+anomaly_end_pos = []
+anomaly_severity = []
+c1,c2,c3 = st.columns(3)
+for anomaly_no in range(no_of_anomalies):
+    with c1:
+        start_pos = st.number_input('start_pos',value=0.0,min_value=0.0, key=f'anomaly_start_{anomaly_no}')
+        anomaly_start_pos.append(start_pos)
+    with c2:
+        end_pos = st.number_input('end_pos',value=100.0,key=f'anomaly_end_{anomaly_no}')
+        anomaly_end_pos.append(end_pos)
+    with c3:
+        severity = st.number_input('severity',value=2,min_value=1,max_value=4,key=f'anomaly_sev_{anomaly_no}')
+        anomaly_severity.append(severity)
+
+defect['start_pos'] = defect_start_pos
+defect['end_pos'] = defect_end_pos
+defect['defect_code_id'] = defect_severity
+defect['defect_id'] = defect.index+1
+st.write(defect)
+
+anomaly['start_pos'] = anomaly_start_pos
+anomaly['end_pos'] = anomaly_end_pos
+anomaly['defect_code_id'] = anomaly_severity
+anomaly['anomaly_id'] = anomaly.index+1
+st.write(anomaly)
+
+line_width = 10
+if st.button('Get recommendations'):
+
+    no_of_scenarios = 1
+    lst1 = ['simulated scenario'] # scenarios['Between last and current inspection'].to_list()[:no_of_scenarios]
+    lst2 = no_of_scenarios*['anomaly-recommendation']
+    lst3 = no_of_scenarios*['defect-recommendation']
+    fig = make_subplots(
+        rows=no_of_scenarios*3, cols=1,  specs=no_of_scenarios*[[{"type": "xy"}], [{"type": "table"}],[{"type": "table"}]],
+        subplot_titles=[item for pair in zip(lst1, lst2,lst3) for item in pair])
+
+    for scenario in range(1,no_of_scenarios+1):   
+            
+        anomaly_recommendation = get_anomaly_recommendation(anomaly, defect, proximity, min_percentage,min_severity_improvement, min_overlap_extent)
+        defect_recommendation = get_defect_recommendation(anomaly_recommendation, defect)
+
+        x_min = min(defect.start_pos.min(),anomaly.start_pos.min()) - proximity
+        x_max = max(defect.end_pos.max(),anomaly.end_pos.max()) + proximity
+
+    #     fig = go.Figure()
+        count_of_traces = 0
+
+        count_list_for_scatter = []
+        for i, row in defect.iterrows():
+
+            # Add a horizontal line with start and end points
+            fig.add_shape(
+                type='line',
+                x0=row.start_pos,
+                y0=count_of_traces,
+                x1=row.end_pos,
+                y1=count_of_traces,
+                opacity = 0.25,
+                line=dict(color='red', width=line_width),
+                row=3*(scenario-1)+1,
+                col=1
+            )
+
+            count_list_for_scatter.append(count_of_traces)
+            count_of_traces = count_of_traces + 1
+
+        # Add traces
+        fig.add_trace(go.Scatter(x=defect.start_pos, y=count_list_for_scatter,
+                            mode='markers',
+                            name='defect', marker=dict(size=line_width,color='green',opacity=0)), row=3*(scenario-1)+1, col=1)
+        # Add traces
+        fig.add_trace(go.Scatter(x=defect.end_pos, y=count_list_for_scatter,
+                            mode='markers',
+                            name='defect', marker=dict(size=line_width,color='green',opacity=0)),row=3*(scenario-1)+1,col=1)
+            # Add traces
+        fig.add_trace(go.Scatter(x=defect.start_pos, y=count_list_for_scatter,
+                            mode='text',
+                            textfont=dict(color="black",size=18,family="Arail",),
+                            text = defect.defect_code_id.apply(lambda x:'Sev-'+str(x)),
+                            name='defect', marker=dict(size=line_width,color='purple')), row=3*(scenario-1)+1, col=1)
+
+        count_list_for_scatter = []
+        for i, row in anomaly.iterrows():
+
+            # Add a horizontal line with start and end points
+            fig.add_shape(
+                type='line',
+                x0=row.start_pos,
+                y0=count_of_traces,
+                x1=row.end_pos,
+                y1=count_of_traces,
+                opacity = 0.25,
+                line=dict(color='blue', width=line_width),
+                row=3*(scenario-1)+1,
+                col=1
+            )
+
+            count_list_for_scatter.append(count_of_traces)
+            count_of_traces = count_of_traces + 1
+
+        # Add traces
+        fig.add_trace(go.Scatter(x=anomaly.start_pos, y=count_list_for_scatter,
+                            mode='markers',
+                            name='anomaly', marker=dict(size=line_width,color='purple',opacity=0)), row=3*(scenario-1)+1, col=1)
+        # Add traces
+        fig.add_trace(go.Scatter(x=anomaly.end_pos, y=count_list_for_scatter,
+                            mode='markers',
+                            name='anomaly', marker=dict(size=line_width,color='purple',opacity=0)), row=3*(scenario-1)+1, col=1)
+        # Add traces
+        fig.add_trace(go.Scatter(x=anomaly.start_pos, y=count_list_for_scatter,
+                            mode='text',
+                            textfont=dict(color="black",size=18,family="Arail",),
+                            text = anomaly.defect_code_id.apply(lambda x:'Sev-'+str(x)),
+                            name='anomaly', marker=dict(size=line_width,color='purple')), row=3*(scenario-1)+1, col=1)
+        
+        table = go.Table(
+        header=dict(values=list(anomaly_recommendation.columns)),
+        cells=dict(values=[anomaly_recommendation[col] for col in anomaly_recommendation.columns])
+    )
+        fig.add_trace(table, row=3*(scenario-1)+2, col=1)
+        
+        table = go.Table(
+        header=dict(values=list(defect_recommendation.columns)),
+        cells=dict(values=[defect_recommendation[col] for col in defect_recommendation.columns])
+    )
+        fig.add_trace(table, row=3*(scenario-1)+3, col=1)
+
+    #     
+
+    # #     # Automatically adjust the x and y axis range
+    # #     fig.update_layout(xaxis_range=[x_min-2*proximity, x_max+proximity], yaxis_range=[-2, count_of_traces+1])
+    # # Hide the legends for each trace
+        fig.update_traces(showlegend=False, row=3*(scenario-1)+1, col=1)
+
+    # Add a title to the plot
+    fig.update_layout(title="Different scenarios, Red Trace: Defect (past inspection), Blue Trace: Anomaly (current inspection)",
+                    height=200*no_of_scenarios*3)
+    fig.update_yaxes(showticklabels=False)
+    fig.update_yaxes(autorange='reversed')
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+
+
+
+
+
+
---
+++ utils_recommendation.py
@@ -0,0 +1,318 @@
+import itertools
+import logging
+
+import numpy as np
+import pandas as pd
+
+from config_logging import setup_logging
+
+# setup logging
+setup_logging(logging, logging.DEBUG)
+_logger = logging.getLogger(__name__)
+
+
+def get_anomaly_recommendation(
+    anomaly: pd.DataFrame,
+    defect: pd.DataFrame,
+    proximity: float = 0,
+    min_percentage: float = 0.5,
+    min_severity_improvement: int = 1,
+    min_overlap_extent: int = 10,
+    anomaly_recommendation_id_start: int = 0,
+):
+    """Returns anomaly_recommendation dataframe
+
+    Args:
+        anomaly (pd.DataFrame): content of anomaly table
+        defect (pd.DataFrame): content of defect table
+        proximity (float): proximity tolerance of anomaly to defect. Defaults to 0.
+        min_percentage (float): anomaly-length/defect range minimum percentage. Defaults to 0.5.
+        min_severity_improvement (int): minimum severity improvement to create a new Defect. Defaults to 1.
+        min_overlap_extent (int): overlap extent in distance. Defaults to 10.
+        anomaly_recommendation_id_start (int, optional): last index of anomaly recommendation table. Defaults to 0.
+
+    Returns:
+        anomaly_recommendation (pd.DataFrame): to be ingested into the anomaly_recommendation table
+    """
+    anomaly_recommendation = pd.DataFrame(
+        columns=[
+            "anomaly_recommendation_id",
+            "anomaly_id",
+            "recommended_action_id",
+            "recommended_defect_id",
+            "user",
+            "modified_dttm",
+        ]
+    )
+
+    if anomaly.shape[0] == 0:
+        return anomaly_recommendation
+
+    # fill values in anomaly recommendation dataframe
+    anomaly_recommendation["anomaly_id"] = anomaly.anomaly_id.values
+    anomaly_recommendation["anomaly_recommendation_id"] = (
+        anomaly_recommendation_id_start + anomaly_recommendation.index
+    )
+
+    recommendations = []
+    recommended_defect_id = []
+
+    if len(defect) == 0:  # if defect doesn't exist
+        _logger.debug("No past defects exist. Let's create new defects.")
+        anomaly_recommendation["recommended_action_id"] = "Create New Defect"
+        return anomaly_recommendation
+
+    # this section will be executed if defects exist
+    _logger.debug("past defects exist")
+    defect["length"] = defect["end_pos"] - defect["start_pos"]
+
+    for _, row in anomaly.iterrows():
+
+        overlapping_index = defect[
+            (
+                (
+                    (defect.start_pos <= row.end_pos + proximity)
+                    & (defect.start_pos >= row.start_pos - proximity)
+                )
+                | (
+                    (defect.end_pos <= row.end_pos + proximity)
+                    & (defect.end_pos >= row.start_pos - proximity)
+                )
+            )
+        ].index.values
+
+        if len(overlapping_index) > 1:
+            # if no. of associated defects > 1,
+            # check for min_overlap_extent as well
+            overlapping_index = defect[
+                (
+                    (
+                        (defect.start_pos <= row.end_pos + proximity)
+                        & (defect.start_pos >= row.start_pos - proximity)
+                    )
+                    | (
+                        (defect.end_pos <= row.end_pos + proximity)
+                        & (defect.end_pos >= row.start_pos - proximity)
+                    )
+                )
+                & (
+                    defect["end_pos"].apply(lambda x: min(x, row.end_pos))
+                    - defect["start_pos"].apply(lambda x: max(x, row.start_pos))
+                    >= min_overlap_extent
+                )
+            ].index.values
+
+        if (len(overlapping_index) == 1) and (
+            row.defect_code_id
+            - defect.loc[overlapping_index]["defect_code_id"].values[0]
+            >= min_severity_improvement
+        ):  # severity has improved
+
+            # create new defect
+            create_string = "Create New Defect"
+            _logger.debug(create_string)
+            recommendations.append(create_string)
+            recommended_defect_id.append(np.nan)
+
+        elif (len(overlapping_index) == 1) and (
+            row.length / defect.loc[overlapping_index]["length"].values[0]
+            < min_percentage
+        ):
+            create_string = "Create New Defect"
+            _logger.debug(create_string)
+            recommendations.append(create_string)
+            recommended_defect_id.append(np.nan)
+
+        elif len(overlapping_index) == 1:  # default scenario
+            past_tag = "Tag to past defect/s"
+            _logger.debug(past_tag)
+            recommendations.append(past_tag)
+            recommended_defect_id.append(
+                list(defect.loc[overlapping_index]["defect_id"])
+            )
+
+        elif (len(overlapping_index) > 1) and (
+            (row.defect_code_id - defect.loc[overlapping_index]["defect_code_id"])
+            >= min_severity_improvement
+        ).all():
+            # all associated defects have severity improvements >= min_severity_improvement
+
+            # create new defects
+            create_string = "Create New Defect"
+            _logger.debug(create_string)
+            recommendations.append(create_string)
+            recommended_defect_id.append(np.nan)
+
+        elif (
+            (len(overlapping_index) > 1)
+            and (
+                (
+                    (
+                        row.defect_code_id
+                        - defect.loc[overlapping_index]["defect_code_id"]
+                        < min_severity_improvement
+                    )
+                ).sum()
+                == 1
+            )
+            and row.length
+            / defect.loc[
+                (
+                    (
+                        row.defect_code_id
+                        - defect.loc[overlapping_index]["defect_code_id"]
+                        < min_severity_improvement
+                    )[
+                        row.defect_code_id
+                        - defect.loc[overlapping_index]["defect_code_id"]
+                        < min_severity_improvement
+                    ].index
+                ),
+                "length",
+            ].values[0]
+            < min_percentage
+        ):
+            # only one defect < min_sev_improvement and anomaly/defect_range < min_percentage
+            create_string = "Create New Defect"
+            _logger.debug(create_string)
+            recommendations.append(create_string)
+            recommended_defect_id.append(np.nan)
+
+        elif (
+            (len(overlapping_index) > 1)
+            and (
+                (
+                    (
+                        row.defect_code_id
+                        - defect.loc[overlapping_index]["defect_code_id"]
+                        < min_severity_improvement
+                    )
+                ).sum()
+                == 1
+            )
+            and row.length
+            / defect.loc[
+                (
+                    (
+                        row.defect_code_id
+                        - defect.loc[overlapping_index]["defect_code_id"]
+                        < min_severity_improvement
+                    )[
+                        row.defect_code_id
+                        - defect.loc[overlapping_index]["defect_code_id"]
+                        < min_severity_improvement
+                    ].index
+                ),
+                "length",
+            ].values[0]
+            >= min_percentage
+        ):
+            # only one defect < min_sev_improvement and anomaly/defect_range >= min_percentage
+            # add to existing defects
+            past_tag = "Tag to past defect/s"
+            _logger.debug(past_tag)
+            recommendations.append(past_tag)
+            past_defects_list = defect.loc[
+                (
+                    row.defect_code_id - defect.loc[overlapping_index]["defect_code_id"]
+                    < min_severity_improvement
+                )[
+                    row.defect_code_id - defect.loc[overlapping_index]["defect_code_id"]
+                    < min_severity_improvement
+                ].index
+            ]["defect_id"].to_list()
+            recommended_defect_id.append(past_defects_list)
+
+        elif (len(overlapping_index) > 1) and (
+            (
+                (
+                    row.defect_code_id - defect.loc[overlapping_index]["defect_code_id"]
+                    < min_severity_improvement
+                )
+            ).sum()
+            > 1
+        ):
+            # more than one defect < min_sev_improvement
+            # add those defects to recommendation whose anomaly-length/defect-range >= min_percentage
+            anomaly_to_defect_ratio = (
+                row.length
+                / defect.loc[
+                    (
+                        (
+                            row.defect_code_id
+                            - defect.loc[overlapping_index]["defect_code_id"]
+                            < min_severity_improvement
+                        )[
+                            (
+                                row.defect_code_id
+                                - defect.loc[overlapping_index]["defect_code_id"]
+                                < min_severity_improvement
+                            )
+                        ].index
+                    ),
+                    "length",
+                ]
+            ) >= min_percentage
+            past_defects_list = (
+                defect.loc[overlapping_index]["defect_id"]
+                .loc[(anomaly_to_defect_ratio[anomaly_to_defect_ratio == True]).index]
+                .to_list()
+            )
+
+            past_tag_dict = {0: "Create New Defect"}
+            past_tag = past_tag_dict.get(len(past_defects_list), "Tag to past defect/s")
+            _logger.debug(past_tag)
+            recommendations.append(past_tag)
+            past_defects_list_dict = {0: np.nan}
+            recommended_defect_id.append(
+                past_defects_list_dict.get(len(past_defects_list), past_defects_list)
+            )
+
+        else:
+            create_string = "Create New Defect"
+            _logger.debug(create_string)
+            recommendations.append(create_string)
+            recommended_defect_id.append(np.nan)
+
+    anomaly_recommendation["recommended_action_id"] = recommendations
+    anomaly_recommendation["recommended_defect_id"] = recommended_defect_id
+
+    return anomaly_recommendation
+
+
+def get_defect_recommendation(
+    anomaly_recommendation: pd.DataFrame, defect: pd.DataFrame
+):
+    """Returns defect_recommendation
+
+    Args:
+        anomaly_recommendation (pd.DataFrame): anomaly recommendations from latest inspection run
+        defect (pd.DataFrame): contents of defect able
+
+    Returns:
+        defect_recommendation (pd.DataFrame): defect recommendation dataframe primarily to close non-existent defects
+    """
+
+    recommended_defects = set(
+        list(
+            itertools.chain(
+                *list(anomaly_recommendation.recommended_defect_id.dropna())
+            )
+        )
+    )
+    existing_defects = set(defect.defect_id)
+    defect_recommendation = pd.DataFrame(
+        columns=[
+            "defect_recommendation_id",
+            "defect_id",
+            "recommended_action_id",
+            "review_status_id",
+            "user",
+            "modified_dttm",
+        ]
+    )
+    defect_recommendation.defect_id = list(existing_defects - recommended_defects)
+    defect_recommendation.recommended_action_id = "Close"
+    defect_recommendation.defect_recommendation_id = defect_recommendation.index
+
+    return defect_recommendation
